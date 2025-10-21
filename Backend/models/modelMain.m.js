dad89edaa7155c440330ebd27752333a1e5d6747
nghi220713 <nghi220713@student.nctu.edu.vn>
2025-10-21 14:09:58 +0700

    update model user from nghi

--- Backend/models/modelMain.m.js
+++ Backend/models/modelMain.m.js
@@ -4,6 +4,19 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
+  password: String,
+  role:{
+    type:String,
+    enum:["admin","user"],
+    default:"user",
+  },
+  avatar: String,
+  status:{
+    type:String,
+    enum:["active","banned"],
+    default:"active",
+}
 });
+
 
 export default mongoose.model("User", userSchema);
