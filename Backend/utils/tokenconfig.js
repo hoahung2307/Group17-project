22e2553dcd716acdbbc8957d1e14e6d400658aed
hoahung2307 <231185300+hoahung2307@users.noreply.github.com>
2025-11-09 20:01:31 +0700

    Update tokenconfig.js

--- Backend/utils/tokenconfig.js
+++ Backend/utils/tokenconfig.js
@@ -10,8 +10,8 @@ export const setToken = (res, token, rememberMe = "false") => {
   const maxAge = rememberMe === "true" ? 30 * 24 * 60 * 60 * 1000 : 3 * 24 * 60 * 60 * 1000;
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "lax",
+    secure: true,
+    sameSite: 'None',
     maxAge
   });
 };
